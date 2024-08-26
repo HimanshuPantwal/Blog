9b8320a0eafc69c293abfbcd705ae1a079427184
HimanshuPantwal <himanshupantwal5@gmail.com>
2024-08-26 15:52:58 +0530

    commit

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -11,8 +11,8 @@ function PostCard({
 }) {
   return (
     <Link to={`/post/${$id}`}>
-       <div className='w-full bg-gray-100 rounded-xl p-4 h-80 flex flex-col justify-center items-center hover:duration-200 hover:scale-110' data-aos='fade-up'>
-        <div className='w-full justify-center mb-4 flex' >
+       <div className='w-full bg-gray-100 rounded-xl p-4 h-full flex flex-col justify-center items-center hover:duration-200 hover:scale-110' data-aos='fade-up'>
+        <div className='w-full justify-center mb-4 flex p-4 bg-green-400' >
             <img src={appwriteService.getFilePreview(featuredImage)} alt="title"
             className='rounded-xl w-11/12 h-60'
             ></img>
