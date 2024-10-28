799da9db2287fb247ddcadf6d4cfa2be7ecdc296
HimanshuPantwal <himanshupantwal5@gmail.com>
2024-10-28 11:09:37 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,7 +40,7 @@ function Home() {
                             <div className="text-5xl font-extrabold text-gray-900 mb-4 font-serif">
                                 Welcome {user.toUpperCase()}
                             </div>
-                            <img src="https://img.freepik.com/free-vector/organic-flat-blog-post-illustration-with-people_23-2148955260.jpg?t=st=1729662234~exp=1729665834~hmac=720c23262f4b01ac385297eb0b1e1b020f4755fef211b5c5ea378c962fefbde4&w=996" />
+                            <img src="https://plus.unsplash.com/premium_vector-1682310595106-ecf4ee316a54?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
                             <p className="text-2xl text-gray-700 mb-6 font-serif">
                                 Share your stories, express your ideas, and connect with the world—one blog at a time.
                             </p>
@@ -60,7 +60,7 @@ function Home() {
                         <div className="text-4xl text-center font-extrabold font-serif absolute top-20  w-full">
                             WELCOME {user.toUpperCase()}
                         </div>
-                        <img src="https://img.freepik.com/free-vector/organic-flat-blog-post-illustration-with-people_23-2148955260.jpg?t=st=1729662234~exp=1729665834~hmac=720c23262f4b01ac385297eb0b1e1b020f4755fef211b5c5ea378c962fefbde4&w=996" />
+                        <img src="https://plus.unsplash.com/premium_vector-1682310595106-ecf4ee316a54?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
                     </div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full p-2" data-aos='fade-left'>
                         {posts.map((post, index) => (
